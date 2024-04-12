401ef95447e19468c988e142843877fcd6f3d31e
Dhananjay <56788799+dhananjay1906@users.noreply.github.com>
2024-04-12 16:26:33 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// script.js
+
+VANTA.BIRDS({
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color1: 0xf2ca6,
+  speedLimit: 4.00,
+  quantity: 4.00
+})
